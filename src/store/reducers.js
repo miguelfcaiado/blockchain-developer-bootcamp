786f1e367dfe45577188b0c9d8113b832b3ca950
miguelfcaiado <caiado.miguelwork@gmail.com>
2023-05-23 18:37:18 +0100

    Redux pt 1

---
+++ src/store/reducers.js
@@ -0,0 +1,42 @@
+export const provider = (state = {}, action) => {
+	switch (action.type) {
+		case 'PROVIDER_LOADED':
+			return {
+				...state,
+				connection: action.connection
+			}
+		case 'NETWORK_LOADED':
+			return{
+				...state,
+				chainId: action.chainId
+			}
+		case 'ACCOUNT_LOADED':
+			return {
+				...state,
+				account: action.account
+			}
+		case 'TOKEN_LOADED':
+			return {
+				...state,
+				token: action.token
+			}
+
+		default:
+			return state
+	}
+}
+
+export const tokens = (state = {loaded: false, contract: null}, action) => {
+	switch (action.type) {
+		case 'TOKEN_LOADED':
+			return {
+				...state,
+				loaded: true,
+				contact: action.token,
+				symbol: action.symbol
+			}
+		
+		default:
+			return state
+	}
+}
